d9bb23cb2ac6588e45afa92624fca09b87c3261a
Zaid Bin Khaliq <zaidbinkhaliq@gmail.com>
2025-02-05 21:08:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const flightNumberInput = document.getElementById('flight-number');
 const submitBtn = document.getElementById('submit-btn');
 const flightStatusDiv = document.getElementById('flight-status');
 
-// AviationStack API endpoint and API key
+// AviationStack API endpoint and API key      
 const apiEndpoint = ' https://api.aviationstack.com/v1/flights';
-const apiKey = 'Your Key'; // Replace with your actual API key
+const apiKey = 'Your  Key'; // Replace with your actual API key
 
 flightForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
+         
     const flightNumber = flightNumberInput.value.trim();
 
     if (flightNumber) {
